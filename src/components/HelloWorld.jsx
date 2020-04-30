0215fddf54f4a3d43f224fb7488c5c7f1a9b6799
DJhonJ <diazj691@gmail.com>
2020-04-29 21:55:02 -0500

    ESLint todo listo

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/styles/App.scss'
+
+class HelloWorld extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <h3>HelloWorld</h3>
+            </div>
+        )
+    }
+}
+
+export default HelloWorld;
