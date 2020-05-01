acbdffaf62809121a070502eb10e285b944f0052
DJhonJ <diazj691@gmail.com>
2020-04-30 19:10:21 -0500

    proyecto terminado, con fake API

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+
+const useInitialState = (API) => {
+    const [ video, setVideo ] = useState([]);
+
+    useEffect(() => {
+        fetch(API)
+            .then(response => response.json())
+            .then(data => setVideo(data))
+    }, [])
+
+    return video;
+}
+
+export default useInitialState;
